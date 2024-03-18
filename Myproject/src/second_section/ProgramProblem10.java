bc5b65f522623d94c58855995c9d9179f4282e6c
김민혁 <evanleo1021@gmail.com>
2024-03-18 22:51:02 +0900

    test1

---
+++ Myproject/src/second_section/ProgramProblem10.java
@@ -0,0 +1,10 @@
+package second_section;
+//자동판매기로부터 물건을 사면 거스름돈을 계산하여 알려주는 프로그램
+public class ProgramProblem10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
