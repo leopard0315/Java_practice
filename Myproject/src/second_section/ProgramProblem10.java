2dc5a2fe7b625763586fb60feae7b70db500524e
김민혁 <evanleo1021@gmail.com>
2024-03-18 23:24:26 +0900

    fix

--- Myproject/src/second_section/ProgramProblem10.java
+++ Myproject/src/second_section/ProgramProblem10.java
@@ -1,9 +1,12 @@
 package second_section;
+import java.util.Scanner;
 //자동판매기로부터 물건을 사면 거스름돈을 계산하여 알려주는 프로그램
 public class ProgramProblem10 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		scan.close();
 
 	}
 
